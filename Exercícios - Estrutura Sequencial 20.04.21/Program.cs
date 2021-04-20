6e63b7700f33afc97b1e2028a1d6f995b3f16f16
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-20 15:08:32 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios - Estrutura Sequencial 20.04.21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercícios___Estrutura_Sequencial_20._04._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
