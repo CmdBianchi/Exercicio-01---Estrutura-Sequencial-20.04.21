eeb0ff316d81221339d73ee59199c02f97f3b4df
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-20 15:43:47 -0300

    Adicionado Função MSG & CALCULO.

--- Exercícios - Estrutura Sequencial 20.04.21/Program.cs
+++ Exercícios - Estrutura Sequencial 20.04.21/Program.cs
@@ -4,9 +4,49 @@ namespace Exercícios___Estrutura_Sequencial_20._04._21
 {
     class Program
     {
+        //-----> INICIO
+        ///////////////////////////////////////////////////////////////////////////////////////////////////     
         static void Main(string[] args)
         {
-            
+            MsgBoasVindas();
+            Calculo();
+
+
         }
+        /////////////////////////////////////////////////////////////////////////////////////////////////// 
+        static void MsgBoasVindas()
+        {
+            Console.WriteLine("Seja bem vindo!");
+            Console.WriteLine("Codigo simples para soma de DOIS numeros");
+            Console.WriteLine(" ");
+        }
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////// 
+        static void Calculo()
+        {
+            int a, b, soma;
+
+            Console.Write("Ecreva valor de A: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Ecreva valor de B: ");
+            b = int.Parse(Console.ReadLine());
+
+            soma = a + b;
+
+            Console.Write("Soma: "+soma);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
